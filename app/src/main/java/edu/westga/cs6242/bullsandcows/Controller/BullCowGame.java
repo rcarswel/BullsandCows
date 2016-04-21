f54acbadb7695b749761f0c479c031b6295d7b21
Robert Carswell <rcarswel@westga.edu>
2016-04-21 18:25:20 -0400

    Added game play controller for players and rules.

---
+++ app/src/main/java/edu/westga/cs6242/bullsandcows/Controller/BullCowGame.java
@@ -0,0 +1,8 @@
+package edu.westga.cs6242.bullsandcows.Controller;
+
+/**
+ * Created by rcarswel on 4/19/2016. Version 1.
+ * Game interactions between players (2) and rules for Bulls and Cows game.
+ */
+public class BullCowGame {
+}
