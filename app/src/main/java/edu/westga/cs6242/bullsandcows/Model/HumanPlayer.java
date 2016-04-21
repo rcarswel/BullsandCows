97af8622cfa128a1b8b17e44178c046a9ad25bf0
Robert Carswell <rcarswel@westga.edu>
2016-04-21 18:15:30 -0400

    Added HumanPlayer.

---
+++ app/src/main/java/edu/westga/cs6242/bullsandcows/Model/HumanPlayer.java
@@ -0,0 +1,14 @@
+package edu.westga.cs6242.bullsandcows.Model;
+
+/**
+ * Created by rcarswel on 4/21/2016. Version 1.
+ * HumanPlayer for Bulls and Cows game.
+ */
+public class HumanPlayer extends AbstractPlayer implements Player {
+
+    /**
+     * Creates a new HumanPlayer.
+     */
+    public HumanPlayer() {
+    }
+}
