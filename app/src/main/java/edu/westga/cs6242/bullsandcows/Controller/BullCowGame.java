ac00741295242c5bf840e79fbc606cde683557e3
Robert Carswell <rcarswel@westga.edu>
2016-04-22 14:52:13 -0400

    Started Game controller, needs work.

--- app/src/main/java/edu/westga/cs6242/bullsandcows/Controller/BullCowGame.java
+++ app/src/main/java/edu/westga/cs6242/bullsandcows/Controller/BullCowGame.java
@@ -1,8 +1,27 @@
 package edu.westga.cs6242.bullsandcows.Controller;
 
+import edu.westga.cs6242.bullsandcows.Model.BullCowRules;
+import edu.westga.cs6242.bullsandcows.Model.ComputerPlayer;
+import edu.westga.cs6242.bullsandcows.Model.HumanPlayer;
+import edu.westga.cs6242.bullsandcows.Model.Player;
+
 /**
  * Created by rcarswel on 4/19/2016. Version 1.
  * Game interactions between players (2) and rules for Bulls and Cows game.
  */
 public class BullCowGame {
+    private BullCowRules rules;
+    private Player playerA;
+    private Player playerB;
+
+    /**
+     * Constructor
+     * Builds rules and two players
+     */
+    public BullCowGame() {
+        this.rules = new BullCowRules();
+        // Current 1 Human/ 1 Computer (may update)
+        this.playerA = new HumanPlayer();
+        this.playerB = new ComputerPlayer();
+    }
 }
