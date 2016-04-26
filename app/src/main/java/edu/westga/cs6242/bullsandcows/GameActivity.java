7d372266a002007e3e337833f55cd9dd1f96f080
rcarswel <rcarswel@westga.edu>
2016-04-26 17:02:42 -0400

    Active work on layouts.

---
+++ app/src/main/java/edu/westga/cs6242/bullsandcows/GameActivity.java
@@ -0,0 +1,7 @@
+package edu.westga.cs6242.bullsandcows;
+
+/**
+ * Created by rcarswel on 4/26/2016.
+ */
+public class GameActivity {
+}
--- app/src/main/java/edu/westga/cs6242/bullsandcows/MainActivity.java
+++ app/src/main/java/edu/westga/cs6242/bullsandcows/MainActivity.java
@@ -1,13 +1,11 @@
 package edu.westga.cs6242.bullsandcows;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -48,5 +35,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Starts the game from main
+     *
+     * @param view current view
+     */
+    public void click_Start(View view) {
+        Intent intent = new Intent(view.getContext(), GameActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * Closes the game from main
+     */
+    public void click_Exit(View view) {
+        finish();
+        System.exit(0);
     }
 }
